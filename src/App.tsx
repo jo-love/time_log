a2cc0785d9e296a86c370c96cb1a9c9435772cb2
yeonjeongCho <jolove.dev@gmail.com>
2022-01-21 18:23:35 +0900

    github 배포 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const NotFound = lazy(() => import('pages/NotFound'));
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Suspense
         fallback={
           <div style={{ textAlign: 'center', marginTop: '30px' }}>
